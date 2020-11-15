3858ba314f6569d72e307df45c2d972d2a057570
liukz <1573524354@qq.com>
2020-11-15 11:11:31 +0800

    gengxin1

--- HelloGit/src/main/java/com/sunreal/git/HelloGit.java
+++ HelloGit/src/main/java/com/sunreal/git/HelloGit.java
@@ -8,5 +8,7 @@ package com.sunreal.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
     }
 }

b280b012c6bc0df4b8ebfc73c2f89c3b1c5715f2
liukz <1573524354@qq.com>
2020-11-15 11:09:45 +0800

    测试git

---
+++ HelloGit/src/main/java/com/sunreal/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.sunreal.git;
+
+/**
+ * @author lkz
+ * @description
+ * @date 2020/11/15 11:01
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
